f7d69754f58dfda2ad8b236ec39bf1ac3e0d9c90
faranidan <idan@ficx.com>
2023-03-19 11:17:13 +0200

    Updates to fix bug of chromedriver v111

--- src/main/java/base/Hooks.java
+++ src/main/java/base/Hooks.java
@@ -10,12 +10,13 @@ public class Hooks extends BasePage {
 	public Hooks() throws IOException {
 		super();
 	}
-	
+
 	@BeforeSuite
 	public void setup() throws InterruptedException, IOException {
+		System.setProperty("webdriver.http.factory", "jdk-http-client");
 		getDriver().get(getUrl());
 	}
-	
+
 	@AfterTest
 	public void end() {
 		WebDriverInstance.cleanupDriver();
